ec4e87d5d2eb93af86ee44a584729eb0bdbfd89d
Shengda <39946188+shengdamao@users.noreply.github.com>
2019-12-29 22:31:01 +0800

    build1
    build1

---
+++ signup/signup/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepage(request):
+    return render(request, 'home.html', {'key1':'value1'})
+
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    print("request_print:")
+    print(request)
+
+    wordlst = fulltext.split()
+    wordcnt = len(wordlst)
+
+    worddict = dict()
+    for word in wordlst:
+        worddict[word] = worddict.get(word, 0) + 1
+    return render(request, 'count.html', {'fulltext': fulltext, 'wordcnt': wordcnt, 'worddict': worddict,
+                                          'items': worddict.items()})
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def sign_up(request):
+    return render(request, 'sign_up.html')
+
+
+def new_game(request):
+    return render(request, 'new_game.html')
+
+def signup_complete(request):
+    return render(request, 'signup_complete.html')
